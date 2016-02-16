564c1c00436c51019660d1d45c98a1ab7a0a6048
Tal <mlcoch.zdenek@seznam.cz>
2016-02-16 20:58:48 +0100

    Initial commit

---
+++ lib/gamee.dart
@@ -0,0 +1,13 @@
+// Copyright (c) 2016, <your name>. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+/// The gamee library.
+///
+/// This is an awesome library. More dartdocs go here.
+library gamee;
+
+// TODO: Export any libraries intended for clients of this package.
+
+export 'src/gamee_base.dart';
+
+part "src/utils.dart";
